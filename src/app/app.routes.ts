7b97fac013f6069fb03ad53172d963f0b50e7d5b
Fitia Lalaina Radavidison <fitialalainarad@gmail.com>
2025-04-03 20:30:24 +0200

    feat: adding 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/home',pathMatch:'full'},
-    {path:'home',loadComponent:()=>import('./home/home.component').then(m => m.HomeComponent)},
+    {path:'',loadComponent:()=>import('./home/home.component').then(m => m.HomeComponent)},
     {path:'login',loadComponent:()=>import('./login/login.component').then(m => m.LoginComponent)},
     {path:'register',loadComponent:()=>import('./register/register.component').then(m => m.RegisterComponent)},
     {path:'**',loadComponent:()=>import('./notfound/notfound.component').then(m => m.NotfoundComponent)},
