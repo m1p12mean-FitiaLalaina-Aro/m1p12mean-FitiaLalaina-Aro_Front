3bce8e80b44ed797aba0eb2ffee5cd7a19c6e436
Fitia Lalaina Radavidison <fitialalainarad@gmail.com>
2025-04-03 19:11:00 +0200

    feat: production config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Fasyl_frontend' title`, () => {
+  it(`should have the 'mada_garage_car' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Fasyl_frontend');
+    expect(app.title).toEqual('mada_garage_car');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Fasyl_frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mada_garage_car');
   });
 });
