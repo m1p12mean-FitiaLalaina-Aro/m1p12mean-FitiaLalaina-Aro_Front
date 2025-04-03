ec6e509561268d00259f48813107848887845de2
Fitia Lalaina Radavidison <fitialalainarad@gmail.com>
2025-04-03 22:02:40 +0200

    feat: adjustement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'home',redirectTo:'',pathMatch:'full'},
     {path:'',loadComponent:()=>import('./home/home.component').then(m => m.HomeComponent)},
     {path:'login',loadComponent:()=>import('./login/login.component').then(m => m.LoginComponent)},
     {path:'register',loadComponent:()=>import('./register/register.component').then(m => m.RegisterComponent)},
